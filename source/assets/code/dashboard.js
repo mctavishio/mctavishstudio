6b614314e4dbca05eea8d7f6f74581c7b440be4a
Kathleen McTavish <kathy@datapoets.com>
2019-12-30 23:45:28 -0600

    trying to figure out sound clip loading ::: removed port

--- source/assets/code/dashboard.js
+++ source/assets/code/dashboard.js
@@ -15,8 +15,9 @@ let createdashboard = z => {
 							let request = new XMLHttpRequest();
 							// request.open("GET", window.location.protocol + "//" + window.location.hostname + ":" + window.location.port + "/" +clip.url, true);
 							// request.open("GET", window.location.protocol + "//" + window.location.hostname + ":" + window.location.port + clip.url, true);
-							request.open("GET", window.location.protocol + "//" + window.location.hostname + ":" + window.location.port + "/" + clip.url, true);
-							z.tools.logmsg("url" + window.location.protocol + "//" + window.location.hostname + ":" + window.location.port + "/"  + clip.url);
+							// request.open("GET", window.location.protocol + "//" + window.location.hostname + ":" + window.location.port + "/" + clip.url, true);
+							request.open("GET", window.location.protocol + "//" + window.location.hostname + "/" + clip.url, true);
+							z.tools.logmsg("url = " + window.location.protocol + "//" + window.location.hostname + "/"  + clip.url);
 							// z.tools.logmsg("trying to load" + clip.url);
 							request.responseType = "arraybuffer";
 							request.onload = () =>  {
